b8e04cab39275eec7617e10fd9b34108d9f43259
sofiaignaciab <sofi.belmar9@gmail.com>
2022-09-12 18:50:20 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,74 @@
 import logo from './logo.svg';
+import Nav from "react-bootstrap/Nav";
+import Routes from "react-bootstrap/Nav";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 import Login from './components/Login';
 import Register from './components/Register';
 import Galery from './components/Galery';
+import { Component } from 'react';
 
-function App() {
+
+function NavScrollExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Galery />
-      </header>
-    </div>
+    <><Navbar bg="dark" variant="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">TICKETERA</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="/home">Inicio</Nav.Link>
+            <Nav.Link href="#action2">Mas vendidos</Nav.Link>
+            <NavDropdown title="Categorias" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Accion</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Terror</NavDropdown.Item>
+              <NavDropdown.Item href="#action5">Romance</NavDropdown.Item>
+              <NavDropdown.Item href="#action6">Drama</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Mi perfil" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="Login.js">Iniciar</NavDropdown.Item>
+              <NavDropdown.Item href="#action8">Registrarse</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Buscar"
+              className="me-2"
+              aria-label="Search" />
+            <Button variant="outline-success">Buscar</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar><div className="App">
+        <header className="App-header">
+          <Galery />
+        </header>
+      </div></>
   );
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <Galery />
+//       </header>
+//     </div>
+//   );
+// }
+
+
+export default NavScrollExample;
--- src/components/Galery.js
+++ src/components/Galery.js
@@ -4,7 +4,7 @@ import { Container, Card, Row, Col, Dropdown, Nav, NavDropdown } from "react-boo
 const Galery = () => {
   return(
     <Container>
-      <Nav className="justify-content-end" defaultActiveKey="/home" as="ul" style={{ fontSize: 23, paddingTop: 15 }}>
+      {/* <Nav className="justify-content-end" defaultActiveKey="/home" as="ul" style={{ fontSize: 23, paddingTop: 15 }}>
         <NavDropdown title="About" id="aboutDropdown">
           <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
           <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
@@ -14,18 +14,18 @@ const Galery = () => {
         </NavDropdown>
 
         <NavDropdown title="My Account" id="accountDropdown">
-          <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
-          <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.1">Mi perfil</NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.2">Historial de compras</NavDropdown.Item>
           <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
           <NavDropdown.Divider />
           <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
         </NavDropdown>
-      </Nav>
+      </Nav> */}
 
       <Row xs={1} md={4} className="g-4" style={{ paddingTop: 15 }} >
         {Array.from({ length: 6 }).map((_, idx) => (
           <Col>
-            <Card className="mb-3" style={{ color: "#001" }} border="secondary">
+            <Card className="mb-3" style={{ color: "#000" }} border="secondary">
               <Card.Img variant="top" src="https://picsum.photos/260/160" />
               <Card.Body>
                 <Card.Title style={{ fontSize: 30 }} > Card title </Card.Title>
@@ -36,7 +36,7 @@ const Galery = () => {
 
                 <Dropdown>
                   <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-                    Fechas
+                    14 enero
                   </Dropdown.Toggle>
 
                   <Dropdown.Menu variant="dark">
@@ -54,5 +54,7 @@ const Galery = () => {
     </Container>
   );
 }
+
+
 
 export default Galery;
