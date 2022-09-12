f350028010d2db7a057a92a956298a4732eb3796
sofiaignaciab <sofi.belmar9@gmail.com>
2022-09-11 22:22:05 -0300

    cambio color

--- src/components/Galery.js
+++ src/components/Galery.js
@@ -25,7 +25,7 @@ const Galery = () => {
       <Row xs={1} md={4} className="g-4" style={{ paddingTop: 15 }} >
         {Array.from({ length: 6 }).map((_, idx) => (
           <Col>
-            <Card className="mb-3" style={{ color: "#000" }} border="secondary">
+            <Card className="mb-3" style={{ color: "#001" }} border="secondary">
               <Card.Img variant="top" src="https://picsum.photos/260/160" />
               <Card.Body>
                 <Card.Title style={{ fontSize: 30 }} > Card title </Card.Title>
