55e001fee0f369aa57f06ea092b9e1ee428b3524
sofiaignaciab <sofi.belmar9@gmail.com>
2022-11-10 22:43:58 -0300

    first commit

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,26 @@
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import {useState} from "react";
+
 const Register = () => {
+    const [userData, setUserData] = useState({
+        name: "",
+        lastname: "",
+        email: "",
+        password: "",
+    });
+
+    const setData = (e) => {
+        const fieldName = e.target.name;
+        setUserData(data => ({
+            ...data,
+            [fieldName]: e.target.value,
+        }))
+    }
+
+    console.log(userData);
 
     const Registration = async(event) => {
         event.preventDefault();
-        const {name, lastName, email, password} = document.forms[0];
-        console.log(name.value, lastName.value, email.value, password.value)
         await fetch('http://localhost:27017/api/users/', {
             method: "POST",
             headers: {
@@ -11,39 +28,52 @@ const Register = () => {
             },
             body: JSON.stringify(
                 {
-                    name: name.value,
-                    lastname: lastName.value,
-                    email: email.value,
-                    password: password.value,
+                    name: userData.name,
+                    lastname: userData.lastname,
+                    email: userData.email,
+                    password: userData.password,
                 }
             )
         });
     }
 
     return (
-        <div>
-            <form>
-                <div>
-                    <label>Nombre</label>
-                    <input type={'text'} name={'name'}/>
-                </div>
-                <div>
-                    <label>Apellido</label>
-                    <input type={'text'} name={'lastName'}/>
-                </div>
-                <div>
-                    <label>Email</label>
-                    <input type={'email'} name={'email'}/>
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input type={'password'} name={'password'}/>
-                </div>
-                <div>
-                    <button type="Submit" onClick={Registration}>Submit</button>
-                </div>
-            </form>
-        </div>
+        <Container>
+            <Col className="d-flex justify-content-center">
+                <Form className="w-50">
+                    <Row>
+                        <Col>
+                            <Form.Group contrsolId="registerName">
+                                <Form.Label> Name </Form.Label>
+                                <Form.Control type="texts" placeholder="Enter Name" name={'name'} onChange={setData} />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group controlId="registerLastName">
+                                <Form.Label> Last Name </Form.Label>
+                                <Form.Control type="text" placeholder="Enter Last Name" name={'lastname'} onChange={setData} />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+
+                    <Col>
+                        <Form.Group controlId="registerEmail">
+                            <Form.Label style={{ textAlign: "left" }} > Email address </Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" name={'email'} onChange={setData} />
+                        </Form.Group>
+
+                        <Form.Group controlId="registerPassword">
+                            <Form.Label> Password </Form.Label>
+                            <Form.Control type="password" placeholder="Password" name={'password'} onChange={setData}/>
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit" onClick={Registration}>
+                            Register
+                        </Button>
+                    </Col>
+                </Form>
+            </Col>
+        </Container>
     )
 }
 
